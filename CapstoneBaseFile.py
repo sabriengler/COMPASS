fa9dfea57c22be03477676ddef2fb497ffec7823
sabriengler <engler.s@northeastern.edu>
2026-03-31 19:32:21 -0400

    Update CapstoneBaseFile.py

--- CapstoneBaseFile.py
+++ CapstoneBaseFile.py
@@ -23,7 +23,7 @@ import pandas as pd
 print ("pands imported")
 from scipy.optimize import minimize_scalar
 print ("scalar imported")
-﻿app = Flask(__name__)
+app = Flask(__name__)
 print("app created")
 app.secret_key = "your_secret_key_here"  # Replace with a secure key in production
 nav_bar = """
@@ -35,12 +35,7 @@ nav_bar = """
 </nav>
 <hr>
 """
-previous_results = []  # Global list to store previous chart results
-﻿
-# ---------------------------
-# Simulation Functions (including optimize_lambda)
-# ---------------------------
-﻿
+previous_results = []  # Global list to store previous chart results﻿
 def generate_behavior_data_sim(behavior, params, n_baseline):
     x = np.arange(n_baseline)
     data_type = params.get('data_type', 'continuous')
